469189a1c854260ac96828d66096b10e516cff5a
nigel norman <nigelnorman_ext@yahoo.com>
2019-10-27 12:16:04 -0400

    adding routing to top and best stories

--- hnchallenge-app/src/app/item-list/item-list.component.ts
+++ hnchallenge-app/src/app/item-list/item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemsService } from '../api/services/items.service';
 import { ItemViewModel } from '../api/models/item-view-model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-list',
@@ -11,16 +12,29 @@ export class ItemListComponent implements OnInit {
 
   public items: ItemViewModel[];
 
-  constructor(private itemsService: ItemsService) {
+  constructor(private itemsService: ItemsService, private router: Router) {
     this.items = [];
   }
 
   ngOnInit() {
-    this.mockLoadItems();
+    this.loadItems();
   }
 
   private loadItems = async () => {
-    this.items = await this.itemsService.GetNew().toPromise();
+    const active = this.router.url;
+    console.log(active);
+
+    switch (active) {
+      case '/newest':
+      this.items = await this.itemsService.GetNew().toPromise();
+      break;
+      case '/best':
+      this.items = await this.itemsService.GetBest().toPromise();
+      break;
+      case '/top':
+      this.items = await this.itemsService.GetTop().toPromise();
+      break;
+    }
   }
 
   private mockLoadItems = () => {
